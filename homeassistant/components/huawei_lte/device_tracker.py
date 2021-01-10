a130634f1f63db5e61388eeed834907ed701690b
Ville Skyttä <ville.skytta@iki.fi>
2021-01-10 23:42:32 +0200

    Try to avoid tracking ourself, the HA instance

--- homeassistant/components/huawei_lte/device_tracker.py
+++ homeassistant/components/huawei_lte/device_tracker.py
@@ -101,6 +101,12 @@ def _is_connected(host: Optional[_HostType]) -> bool:
     )
 
 
+def _is_us(host: _HostType) -> bool:
+    """Try to determine if the host entry is us, the HA instance."""
+    # LAN host info entries have an "isLocalDevice" property, "1" / "0"; WLAN host list ones don't.
+    return cast(Optional[str], host.get("isLocalDevice", "0")) == "1"
+
+
 @callback
 def async_add_new_entities(
     hass: HomeAssistantType,
@@ -116,7 +122,12 @@ def async_add_new_entities(
 
     new_entities: List[Entity] = []
     for host in (
-        x for x in hosts if _is_connected(x) and _is_wireless(x) and x.get("MacAddress")
+        x
+        for x in hosts
+        if not _is_us(x)
+        and _is_connected(x)
+        and _is_wireless(x)
+        and x.get("MacAddress")
     ):
         entity = HuaweiLteScannerEntity(router, host["MacAddress"])
         if entity.unique_id in tracked:
